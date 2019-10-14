8a0bd7fba40343d86ecd4292e622466a0c9a0a82
ToyasDhake <toyasdhake@gmail.com>
2019-10-13 22:16:13 -0400

    [2.4] Adding stub function implementation for the complete pipeline. Updated cmke to include the shared opencv and dlib libraries on the system. [1.3] Also added UML diagram for the pipeline.

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,52 @@
-#include <iostream>
-#include <lib.hpp>
+/**
+* @file main.cpp
+* @author Toyas Dhake (Driver), Shivam Akhauri (Navigator)
+* @date 11 October 2019
+* @copyright 2019 Toyas Dhake, Shivam Akhauri
+* @brief Main file for implementation of the Depth Perception project.
+*/
 
-int main()
-{
-    dummy();
+#include <dlib/opencv.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <dlib/gui_widgets.h>
+#include <distance.hpp>
+#include <face.hpp>
+#include <iostream>
+
+int main() {
+    CalculateDistance calculateDistance;
+    // Initialize camera
+    cv::VideoCapture cap(0);
+    // Check camera is open
+    if (!cap.isOpened())
+    {
+        std::cout << "Unable to connect to camera" << std::endl;
+        return 1;
+    }
+    dlib::image_window win;
+    // Until window is open
+    while(!win.is_closed())
+    {
+        // Grab a frame
+        cv::Mat temp;
+        if (!cap.read(temp))
+        {
+            break;
+        }
+        // Create object of Acme robotics Depth Preception module
+        CalculateDistance calculateDistance;
+        /**
+        * Loactions and distances of faces are stored in vector<Face> can be 
+        * either use to compute something else or add overlay on gui window.
+        */
+        std::vector<Face> faces = calculateDistance.getDistance(temp);
+        /**
+        * There are 2 options for gui, opencv or dlib we are using dlib hence
+        * cv::Mat needs to be converted to dlib::cv_image.
+        */
+        dlib::cv_image<dlib::bgr_pixel> cimg(temp);
+        win.clear_overlay();
+        win.set_image(cimg);
+    }
     return 0;
 }
---
+++ include/distance.hpp
@@ -0,0 +1,28 @@
+/**
+* @file distance.hpp
+* @author Toyas Dhake (Driver), Shivam Akhauri (Navigator)
+* @date 11 October 2019
+* @copyright 2019 Toyas Dhake, Shivam Akhauri
+* @brief This is a class for depth perception module based on image from single 
+* camera. 
+*/
+
+#ifndef INCLUDE_DISTANCE_HPP_
+#define INCLUDE_DISTANCE_HPP_
+
+#include <opencv2/opencv.hpp>
+#include <face.hpp>
+
+class CalculateDistance {
+ private:
+    double knownWidth = 7.0;
+    double knownDistance = 36.0;
+    double focalLength = 1;
+ public:
+    double calculateFocalLength();
+    CalculateDistance();
+    std::vector<Face> getDistance(cv::Mat image);
+    float calDist(float width,float focalLength);
+};
+
+#endif /* INCLUDE_DISTANCE_HPP_ */
